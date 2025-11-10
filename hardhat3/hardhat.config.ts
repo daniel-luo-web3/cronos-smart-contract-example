3ba2d0fbc76e0a8f86ee916574ee63a610a1c1a0
daniel.luo@crypto.com <daniel.luo@crypto.com>
2025-11-10 17:19:51 +0800

    append hardhat ignition verify

--- hardhat3/hardhat.config.ts
+++ hardhat3/hardhat.config.ts
@@ -1,6 +1,19 @@
 import { configVariable, task, type HardhatUserConfig } from "hardhat/config";
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 
+const getExplorerApiKey = (chainId: number) => {
+  let apiKey;
+  if (chainId === 25) {
+    apiKey = configVariable("CRONOS_EXPLORER_MAINNET_API_KEY");
+  } else if (chainId === 338) {
+    apiKey = configVariable("CRONOS_EXPLORER_TESTNET_API_KEY");
+  }
+  if (!apiKey) {
+    throw Error("Explorer API Key Not Set for chainId: " + chainId);
+  }
+  return apiKey;
+}
+
 const getHDWallet = () => {
   const PRIVATE_KEY = configVariable("PRIVATE_KEY");
   if (PRIVATE_KEY) {
@@ -15,6 +28,12 @@ const config: HardhatUserConfig = {
     profiles: {
       default: {
         version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
       production: {
         version: "0.8.28",
@@ -28,11 +47,6 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    development: {
-      type: "http",
-      url: "http://localhost:8545",
-      accounts: getHDWallet(),
-    },
     testnet: {
       chainId: 338,
       type: "http",
@@ -46,7 +60,36 @@ const config: HardhatUserConfig = {
       url: "https://evm.cronos.org/",
       accounts: getHDWallet(),
       chainType: "l1",
-    }
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: getExplorerApiKey(338),
+    },
+  },
+  chainDescriptors: {
+    25: {
+      name: "mainnet",
+      chainType: "l1",
+      blockExplorers: {
+        etherscan: {
+          name: "mainnet",
+          url: "https://explorer.cronos.org",
+          apiUrl: "https://explorer-api.cronos.org/mainnet/api/v1/hardhat/contract"
+        },
+      },
+    },
+    338: {
+      name: "testnet",
+      chainType: "l1",
+      blockExplorers: {
+        etherscan: {
+          name: "testnet",
+          url: "https://explorer.cronos.org/testnet",
+          apiUrl: "https://explorer-api.cronos.org/testnet/api/v1/hardhat/contract"
+        },
+      },
+    },
   }
 };
 
